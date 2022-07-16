a45e43f090414f5873c1b146e7099af56c099480
Antoine Famibelle <antoinefamibelle@gmail.com>
2022-07-16 21:29:48 +0200

    [✨ FEAT]  (View) We can now click on a filter to remove it
    When you click on a filter you are setting it. If the filter is already applied clicking on it will remove it
    from the filters list. I've also remove the useless import for the futur.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
 import "./styles.css";
-import Recipes from "./Recipes/Recipes";
-import {
-  allRecipes,
-  sugarRecipes,
-  summerRecipes,
-  dessertRecipes,
-  noEggsRecipes,
-  chocolateRecipes,
-  autumnRecipes,
-  veganRecipes,
-  chocolateDessertRecipes,
-} from "./Recipes/RecipeData";
 import { TagList } from "./Tagfilter/ListeDesTags";
 import { tagList } from "./Tags/TagData";
 
 export default function App() {
-  const [filter, setFilter] = useState<string>("all");
+  const [filters, setFilters] = useState<Array<string>>([]);
+
+  const manageFilters = (filter: string) => {
+    if (filters.includes(filter)) {
+      setFilters(filters.filter((f) => f !== filter));
+
+    }
+    else {
+      let newFilters: Array<string> = [...filters, filter];
+      setFilters(newFilters);
+    }
+
+  }
   return (
     <div className="App">
       Liste des recettes
@@ -25,10 +25,8 @@ export default function App() {
         {tagList.map((item) => {
           return (
             <button
-            key={item.id}
-              onClick={() => {
-                setFilter(String(item.name.en)
-              )}}
+              key={item.id}
+              onClick={() => { manageFilters(item.name.en)}}
             >
               {item.name.en}
             </button>
@@ -36,17 +34,6 @@ export default function App() {
         }
       )}
       </div>
-      {filter === "all" && <Recipes recipes={allRecipes} />}
-      {filter === "sugar" && <Recipes recipes={sugarRecipes} />}
-      {filter === "summer" && <Recipes recipes={summerRecipes} />}
-      {filter === "dessert" && <Recipes recipes={dessertRecipes} />}
-      {filter === "chocolate dessert" && (
-        <Recipes recipes={chocolateDessertRecipes} />
-      )}
-      {filter === "eggs free" && <Recipes recipes={noEggsRecipes} />}
-      {filter === "chocolate" && <Recipes recipes={chocolateRecipes} />}
-      {filter === "autumn" && <Recipes recipes={autumnRecipes} />}
-      {filter === "vegan" && <Recipes recipes={veganRecipes} />}
     </div>
   );
 }
