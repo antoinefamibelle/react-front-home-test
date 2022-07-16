d2708bbde517d15af90b49ca98dd79605a0ee1f1
Antoine Famibelle <antoinefamibelle@gmail.com>
2022-07-16 22:35:50 +0200

    [✨ FEAT]  (Component) Creating the filtering system
    You can now use as much filter as you want.

--- src/Recipes/Recipes.tsx
+++ src/Recipes/Recipes.tsx
@@ -1,7 +1,26 @@
-import { ListOfRecipes } from "./RecipeType";
+import { ListOfRecipes, RecipeType } from "./RecipeType";
+import { allRecipes } from "./RecipeData";
 import Recipe from "./Recipe";
 
-export const Recipes = ({ recipes }: { recipes: ListOfRecipes }) => {
+export const Recipes = ({ filters }: { filters: Array<string> }) => {
+  const filteredRecipes = () => {
+    let filtered: ListOfRecipes = [];
+    allRecipes.forEach((recipe) => {
+      let numberOfMatch = 0; // number of tags matching the filters
+      recipe.tags.forEach((tag) => {
+        if (filters.includes(tag.name.en)) {
+          numberOfMatch += 1;
+        }
+      });
+      if (!filtered.includes(recipe) && numberOfMatch === filters.length) {
+        filtered.push(recipe);
+      }
+    });
+    if (filters.length === 0) {
+      return allRecipes;
+    }
+    return filtered;
+  }
   return (
     <div
       style={{
@@ -9,20 +28,18 @@ export const Recipes = ({ recipes }: { recipes: ListOfRecipes }) => {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
+        backgroundColor: "whitesmoke",
+        borderWidth: "10px",
+        borderColor: "black",
+        maxWidth: "800px",
       }}
     >
-      <div
-        style={{
-          backgroundColor: "whitesmoke",
-          borderWidth: "10px",
-          borderColor: "black",
-          maxWidth: "800px",
-        }}
-      >
-        {recipes.map((obj) => (
-          <Recipe recipe={obj} />
-        ))}
-      </div>
+      {filteredRecipes().length === 0 && (
+        <div>Aucune recette ne correspond à vos filtres</div>
+      )}
+      {filteredRecipes().map((recipe) => (
+        <Recipe key={recipe.nom} recipe={recipe} />
+      ))}
     </div>
   );
 };
