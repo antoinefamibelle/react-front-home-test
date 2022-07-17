e078e237ade8f6c1282c07e6e2c508f111a32f11
Antoine Famibelle <antoinefamibelle@gmail.com>
2022-07-17 20:41:52 +0200

    [🐛 FIX]  (Component) Fixing commit problem

--- src/Recipes/style/index.ts
+++ src/Recipes/style/index.ts
@@ -2,17 +2,50 @@ import styled from "styled-components"
 
 export const Container = styled.div`
   display: flex;
-  flexDirection: row;
+  flex-direction: row;
   padding: 10px;
-  boxShadow: 5px 5px 7px 2px rgba(0,0,0,0.76);
+  box-shadow: 5px 5px 7px 2px rgba(0,0,0,0.76);
   marginTop: 20px;
+`;
+
+export const CardContainer = styled.div`
+  padding: 10px;
+  background-color: lightblue;
+  width: 100%;
 `;
 
 export const ImageContainer = styled.div`
   display: flex;
-  flexDirection: column;
-  justifyContent: space-evenly;
+  flex-direction: column;
+  justify-content: space-evenly;
   height: 200px;
   width: 70%;
-  backgroundSize: cover;
+  background-size: cover;
+`;
+
+export const RecipeName = styled.h2``;
+
+export const RecipeDescription = styled.div`
+  padding: 10px;
+`;
+
+export const TagContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const IngredientContainer = styled.div`
+  background-color: blanchedalmond;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const StepContainer = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-content: flex-start;
+  align-items: flex-start;
 `;
